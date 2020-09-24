7f35cde3c90d7c3efad16aef50c26f0dbb13abec
Bereket Girma <bgirma@Berekets-MacBook-Pro.local>
2020-09-24 07:17:25 -0700

    final

--- game.js
+++ game.js
@@ -1,52 +1,56 @@
 import { commonWords } from "./constants.js";
 
-// random word more than 3 leeters
-let maxWrongAnswers = 8;
+
+
+// random word more than 3 letters generator
+
 let randomWord = commonWords.filter((words) => words.length >= 3);
 let word = randomWord[Math.floor(Math.random() * randomWord.length)];
 console.log(word);
+
 //  create button for alphabet
-let buttonsForAlphabet = "abcdefghijklmnopqrstuvwxyz"
-  .split("")
-  .map((letter) => `<button id="${letter}">${letter}</button>`)
-  .join("");
-document.querySelector("#keyboard").innerHTML = buttonsForAlphabet;
-// create dashes
+
+let buttonsHTML = "abcdefghijklmnopqrstuvwxyz"
+  .split("")
+  .map((letter) => `<button class="letters" id="${letter}">${letter}</button>`)
+  .join("");
+
+document.querySelector("#keyboard").innerHTML = buttonsHTML;
+
+// dashes
 let answerArray = [];
 for (let i = 0; i < word.length; i++) {
-  answerArray[i] = " _ ";
+  answerArray[i] = "_";
 }
+
 document.querySelector("#dashes").innerHTML = answerArray.join(" ");
-// replacing dash with letter if guessed right
+
+// turn remaining
+let maxWrongAnswers = 8;
+document.querySelector("#guessedLetters").innerHTML = `Remaining Turn: ${maxWrongAnswers}`;;
+
+// var to keep track of correct guessedLetters
+let counter = 0;
+
+// dashes replaced by letter 
 document.querySelector("#keyboard").addEventListener("click", (e) => {
-  let guess = e.target.id;
-  for (let j = 0; j < word.length; j++) {
-    if (word[j] === guess) {
-      answerArray[j] = guess;
-      document.querySelector("#dashes").innerHTML = answerArray.join(" ");
-    }
-  }
-
-  if (word[j] !== guess) {
-    maxWrongAnswers = maxWrongAnswers - 1;
-    document.querySelector(
-      "#guessedLetters"
-    ).innerHTML = `Turns Remaining: ${maxWrongAnswers}`;
-  } else if (word[j] === guess) {
-    maxWrongAnswers = maxWrongAnswers;
-    document.querySelector(
-      "#guessedLetters"
-    ).innerHTML = `Turns Remaining: ${maxWrongAnswers}`;
-  }
-
-  // When the user runs out of turns, show a losing screen
-  if (maxWrongAnswers === 0) {
-    document.querySelector("#gameOver").innerHTML = "Play is Over!";
-  }
+  e.target.disabled = true;
+  let guess = e.target.id;
+  for (let j = 0; j < word.length; j++) {
+    if (word[j] === guess) {
+      answerArray[j] = guess;
+      document.querySelector("#dashes").innerHTML = answerArray.join(" ");
+      counter += 1;
+    }
+  }
+  if (word[j] !== guess) {
+    maxWrongAnswers = maxWrongAnswers - 1;
+    document.querySelector("#guessedLetters").innerHTML = `"Remaining Turn": ${maxWrongAnswers}`;
+  }
+  if (maxWrongAnswers === 0) {
+    document.querySelector("#gameLost").innerHTML = "Play is Over";
+  }
+  if (counter === answerArray.length) {
+    document.querySelector("#gameLost").innerHTML = "You Win";
+  }
 });
-
-// turn remaining,
-// let maxWrongAnswers = 8;
-document.querySelector(
-  "#guessedLetters"
-).innerHTML = `Turns Remaining: ${maxWrongAnswers}`;
